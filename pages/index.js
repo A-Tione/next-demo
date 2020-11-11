0a7863d975c966688ae44195d137620b494811d7
A-Tione <heyanggo@gmail.com>
2020-11-11 15:32:50 +0800

    hello world

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Hello World 大傻
         </h1>
 
         <p className={styles.description}>
